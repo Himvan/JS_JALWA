c2498acdc078b057a9e818bb8e9dda498c0253a2
h0s07uh <Himvan.Sharma@walmart.com>
2022-10-18 10:54:22 +0530

    leyt var const tutorial

---
+++ Let_Var_Const/script.js
@@ -0,0 +1,34 @@
+//  LET VAR CONST
+
+
+ // Before ES6 (2015)
+ 
+    // var myChannel;
+    // myChannel = 'JS JALWA';
+
+ // After ES6 
+// LET
+    // let myChannel;
+    // myChannel = 'JS JALWA'
+
+    // let fullName = 'Himvan'
+    // console.log(myChannel, fullName)
+
+    // let x = 1, y = 2, z = 0;
+    // let a = x * y
+    // console.log(a)
+
+
+// CONST
+
+    // const PI = 3.14;
+    // PI = 3;
+
+
+
+// DIFFERENCES
+    // 1. Scoping Chain (var is a functional scoped and let, const is block scoped)
+    // 2. Hoisting (variable declared with var can be hoisted)
+
+
+  
