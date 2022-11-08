e8c63098ad4c2aee3a7ec962b70aa89a7504d80b
h0s07uh <Himvan.Sharma@walmart.com>
2022-11-08 14:28:33 +0530

    type coersion and conversion tutorial

---
+++ Type_CoersionNConversion/scripts.js
@@ -0,0 +1,43 @@
+'use strict'
+
+/**
+ * Type Conversion OR Explicit Conversion
+ */
+
+    const birthYear = '1997';
+    const age = 24
+
+    // console.log(Number(birthYear) + age);
+
+    // const myChannel = 'js jalwa';
+    // console.log(Number('js jalwa'))
+
+    // console.log(typeof String(age), typeof age);
+
+    // console.log(Boolean(""))
+
+
+/**
+ * Type Coersion OR Implicit Conversion
+ */
+
+    // const mySelf = 'I am ' + age + ' Years Old'; // 'I am ' + String(age) + ' Year Old'
+    // console.log(mySelf);
+
+    // const subsCoersion = '20' - '3' - 15;  // Number(20) - Number(3) - 15
+    // console.log(subsCoersion)
+
+    // console.log('3' * '4')
+    // console.log('20' / '5')
+
+    // console.log('20' - 'Himvan') // Number(20) - Number('Himvan')
+
+
+/**
+ * Equality with Type Conversion 
+ */
+
+
+    // console.log(null == undefined)
+    // console.log(0 == false)
+    // console.log("0" == false)  // 0 == 0
